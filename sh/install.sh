6d3ce691e235580a90e29bd99f4dfacc8acc9f98
Bruno Stabile <brn.stabile@gmail.com>
2015-02-26 04:33:53 +0000

    shell script fixed

--- sh/install.sh
+++ sh/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ##CONFIGURACAO DAS PAGINAS WEB
-ln -s -T /opt/weblabmotor/web/ /www/pages/home/
+ln -s -T /opt/weblabmotor/web/ /www/pages/home
 chown -R www-data.www-data /www/pages/home
 chmod -R 755 /www/pages/home
