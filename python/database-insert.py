afedb40df5c2ff5a1f4ce2947f62faaaddac7a8d
Bruno Stabile <brn.stabile@gmail.com>
2015-07-15 09:11:31 +0000

    added d0 and d1

--- python/database-insert.py
+++ python/database-insert.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-#python inserirDados.py ./database.db a0 a1 a2 a3 a4 a5 a6
+#python inserirDados.py ./database.db a0 a1 a2 a3 a4 a5 d0 d1
 
 import sqlite3, sys
 
@@ -10,12 +10,15 @@ a2=sys.argv[4];
 a3=sys.argv[5];
 a4=sys.argv[6];
 a5=sys.argv[7];
+d0=sys.argv[8];
+d1=sys.argv[9];
+
 
 conn = sqlite3.connect(bancoDados)
 c = conn.cursor()                            
 
 # Insert a row of data
-c.execute("INSERT INTO sensores VALUES (datetime('now','localtime'),?,?,?,?,?,?,'1','0')",(a0,a1,a2,a3,a4,a5))
+c.execute("INSERT INTO sensores VALUES (datetime('now','localtime'),?,?,?,?,?,?,?,?)",(a0,a1,a2,a3,a4,a5,d0,d1))
 
 # Save (commit) the changes
 conn.commit()
