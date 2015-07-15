367c9b6435ca742c4c42b13097eeeb5f3dec34c4
Bruno Stabile <brn.stabile@gmail.com>
2015-07-15 01:33:26 +0000

    install nodejs added

--- sh/install.sh
+++ sh/install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+##DIRETORIOS DE BANCO DE DADOS
 mkdir /tmp/weblabmotor/
 mkdir /var/lib/weblabmotor/
 chown -R www-data.www-data /var/lib/weblabmotor/
@@ -6,17 +8,22 @@ chown -R www-data.www-data /tmp/weblabmotor/
 chmod -R 777 /tmp/weblabmotor/
 chmod -R 777 /var/lib/weblabmotor/
 
+##CONFIGURACAO DAS PAGINAS WEB
 ln -s -T /opt/weblabmotor/web/graficos /www/pages/graficos    
 ln -s -T /opt/weblabmotor/web/cgi-bin /www/pages/cgi-bin    
-ln -s -T /opt/weblabmotor/web/admin /www/pages/admin   
-
+ln -s -T /opt/weblabmotor/web/admin /www/pages/admin  
+ln -s -T /opt/weblabmotor/web/nodejs /www/pages/nodejs
+ 
 chown -R www-data.www-data /www/pages/graficos
 chown -R www-data.www-data /www/pages/cgi-bin
 chown -R www-data.www-data /www/pages/admin
-chown -R 755 /www/pages/graficos
-chown -R 755 /www/pages/cgi-bin
-chown -R 755 /www/pages/admin
+chown -R www-data.www-data /www/pages/nodejs
+
+chmod -R 755 /www/pages/graficos
+chmod -R 755 /www/pages/cgi-bin
+chmod -R 755 /www/pages/admin
+chmod -R 755 /www/pages/admin                   
 
 chown -R www-data.www-data /opt/weblabmotor/web/
-chown -R 755 /opt/weblabmotor/web/
+chmod -R 755 /opt/weblabmotor/web/
 
