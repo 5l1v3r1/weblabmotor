1bbdabb05e5917dae3db5399855ac10e47c6b0b9
Bruno Stabile <brn.stabile@gmail.com>
2015-07-21 20:31:03 -0300

    CSR barometer and accelerommeter addded

--- ino/CentralSensoriamentoRemoto/CentralSensoriamentoRemoto.ino
+++ ino/CentralSensoriamentoRemoto/CentralSensoriamentoRemoto.ino
@@ -25,40 +25,73 @@
    ----------------------------------------------------------
    
 */
-
-
-#include <SPI.h>
-#include <Ethernet.h>
+#include "I2Cdev.h"
+#include "MPU6050.h"
+#include "BMP085.h"
+#include "Wire.h"
+#include "SPI.h"
+#include "Ethernet.h"
 
 //Endereco MAC para o shield Ethernet
 byte mac[] = { 0x98, 0x4F, 0xEE, 0x00, 0x25, 0x6F };
-
-//Pino de controle do motor
-int motor = 6;
-int statusMotor;
+EthernetClient client;
+EthernetServer server(10000);
 
 //Mensagem enviada pela comunicacao ethernet para controlar o experimento
 char c = 0;
 char command = 0;
+
+//Pino de controle do motor
+int motor = 6;
+int statusMotor;
 
 //Flags
 //Comando recebido com sucesso
 boolean runCommand = false;
 boolean flagReadSensors = false;
 
-//Gets a client that is connected to the server and has data available for reading
-EthernetClient client;
+//Barometro
+float temperature;
+float pressure;
+float altitude;
+// class default I2C address is 0x77
+// specific I2C addresses may be passed as a parameter here
+// (though the BMP085 supports only one address)
+BMP085 barometer;
 
-// Acesso ao shield ethernet pela porta 10000
-EthernetServer server(10000);
+//Acelerometro
+int32_t lastMicros;
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
 
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default for InvenSense evaluation board)
+// AD0 high = 0x69
+MPU6050 accelgyro;
+//MPU6050 accelgyro(0x69); // <-- use for AD0 high
+
+
+// uncomment "OUTPUT_READABLE_ACCELGYRO" if you want to see a tab-separated
+// list of the accel X/Y/Z and then gyro X/Y/Z values in decimal. Easy to read,
+// not so easy to parse, and slow(er) over UART.
+#define OUTPUT_READABLE_ACCELGYRO
+
+// uncomment "OUTPUT_BINARY_ACCELGYRO" to send all 6 axes of data as 16-bit
+// binary, one right after the other. This is very fast (as fast as possible
+// without compression or data loss), and easy to parse, but impossible to read
+// for a human.
+//#define OUTPUT_BINARY_ACCELGYRO
+
+
+//..................................MAIN.....................................
 void setup() {
 
   // Inicia-se a comunicaço serial e aguarda uma resposta
   Serial.begin(9600);
 
-  pinSetup();
-  conectarRede();
+  setupPin();
+  setupRede();
 
 }
 
@@ -77,15 +110,72 @@ void loop() {
     readSensors(client);    
   }
 }
+//....................................MAIN...................................
 
+//.......................................SETUP........................................
 
-void pinSetup(){
+void setupPin(){
   
   pinMode(motor,OUTPUT);
   digitalWrite(motor,LOW);
 
 }
 
+void setupRede(){
+  
+  if(!Ethernet.begin(mac)){
+    Serial.println("conectando com a rede...");
+    Ethernet.begin(mac);
+    delay(1000);
+  }
+  delay(200);
+  system("/etc/init.d/networking restart");
+  delay(200);
+  server.begin();
+  delay(200);
+  Serial.print("IP: ");
+  Serial.println(Ethernet.localIP());
+  
+}
+
+void setupI2C(){
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    // initialize serial communication
+    // (38400 chosen because it works as well at 8MHz as it does at 16MHz, but
+    // it's really up to you depending on your project)
+    Serial.begin(115200);
+
+    // initialize device
+    Serial.println("Initializing I2C devices...");
+    
+}
+
+void setupBarometro() {
+    barometer.initialize();
+
+    // verify connection
+    Serial.println("Testing device connections...");
+    Serial.println(barometer.testConnection() ? "BMP085 connection successful" : "BMP085 connection failed");
+
+}
+
+void setupMovimento() {
+  
+    accelgyro.initialize();
+
+    // verify connection
+    Serial.println("Testing device connections...");
+    Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+}
+//.....................................SETUP...........................................
+
+//..................................................
 void comandos(){
   
   if(runCommand){
@@ -206,28 +296,71 @@ statusMotor = digitalRead(motor);
   }
 }
 
-void conectarRede(){
-  
-  if(!Ethernet.begin(mac)){
-    Serial.println("conectando com a rede...");
-    Ethernet.begin(mac);
-    delay(1000);
-  }
-  delay(200);
-  system("/etc/init.d/networking restart");
-  delay(200);
-  server.begin();
-  delay(200);
-  Serial.print("IP: ");
-  Serial.println(Ethernet.localIP());
-  
-}
-
 void resetExperimento(){
 
   Serial.println("Reiniciando experimento...");
-  conectarRede();
-  pinSetup();
+  setup();
   
 }
+
+void barometro() {
+    // request temperature
+    barometer.setControl(BMP085_MODE_TEMPERATURE);
+    
+    // wait appropriate time for conversion (4.5ms delay)
+    lastMicros = micros();
+    while (micros() - lastMicros < barometer.getMeasureDelayMicroseconds());
+
+    // read calibrated temperature value in degrees Celsius
+    temperature = barometer.getTemperatureC();
+
+    // request pressure (3x oversampling mode, high detail, 23.5ms delay)
+    barometer.setControl(BMP085_MODE_PRESSURE_3);
+    while (micros() - lastMicros < barometer.getMeasureDelayMicroseconds());
+
+    // read calibrated pressure value in Pascals (Pa)
+    pressure = barometer.getPressure();
+
+    // calculate absolute altitude in meters based on known pressure
+    // (may pass a second "sea level pressure" parameter here,
+    // otherwise uses the standard value of 101325 Pa)
+    altitude = barometer.getAltitude(pressure);
+
+    // display measured values if appropriate
+    Serial.print("T/P/A\t");
+    Serial.print(temperature); Serial.print("\t");
+    Serial.print(pressure); Serial.print("\t");
+    Serial.print(altitude);
+    Serial.println("");
+}
+
+void movimento() {
+    // read raw accel/gyro measurements from device
+    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+    // these methods (and a few others) are also available
+    //accelgyro.getAcceleration(&ax, &ay, &az);
+    //accelgyro.getRotation(&gx, &gy, &gz);
+
+    #ifdef OUTPUT_READABLE_ACCELGYRO
+        // display tab-separated accel/gyro x/y/z values
+        Serial.print("a/g:\t");
+        Serial.print(ax); Serial.print("\t");
+        Serial.print(ay); Serial.print("\t");
+        Serial.print(az); Serial.print("\t");
+        Serial.print(gx); Serial.print("\t");
+        Serial.print(gy); Serial.print("\t");
+        Serial.println(gz);
+    #endif
+
+    #ifdef OUTPUT_BINARY_ACCELGYRO
+        Serial.write((uint8_t)(ax >> 8)); Serial.write((uint8_t)(ax & 0xFF));
+        Serial.write((uint8_t)(ay >> 8)); Serial.write((uint8_t)(ay & 0xFF));
+        Serial.write((uint8_t)(az >> 8)); Serial.write((uint8_t)(az & 0xFF));
+        Serial.write((uint8_t)(gx >> 8)); Serial.write((uint8_t)(gx & 0xFF));
+        Serial.write((uint8_t)(gy >> 8)); Serial.write((uint8_t)(gy & 0xFF));
+        Serial.write((uint8_t)(gz >> 8)); Serial.write((uint8_t)(gz & 0xFF));
+    #endif
+}
+
 
