34bce7b570d98267fb1841aa570f1fa3c73cf754
Bruno Stabile <brn.stabile@gmail.com>
2015-07-23 20:44:49 -0300

    arduino comm improved

--- weblab/server/java/src/com/example/weblab/ArduinoEthernetComm.java
+++ weblab/server/java/src/com/example/weblab/ArduinoEthernetComm.java
@@ -1,8 +1,6 @@
 package com.example.weblab;
 
-import java.io.BufferedReader;
 import java.io.DataOutputStream;
-import java.io.InputStreamReader;
 import java.net.Socket;
 
 
@@ -16,7 +14,7 @@ public class ArduinoEthernetComm
 	public void CommEthArduino(String serverIP, int serverPort, String msgToServer) throws Exception
 	 {
 		  
-		  String msgFromServer="Nada recebido do experimento";//received message will be stored here  
+		  //String msgFromServer="Nada recebido do experimento";//received message will be stored here  
 
 		  Socket clientSocket = new Socket(serverIP, serverPort);//making the socket connection
 		  System.out.println("Connected to:"+serverIP+" on port:"+serverPort);//debug
@@ -25,16 +23,16 @@ public class ArduinoEthernetComm
 		  DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
 		  
 		  //BufferedReader from Arduino-Server
-		  BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));//
+		  //BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));//
 
 		  outToServer.writeBytes(msgToServer+'\n');//sending the message
 		  //outToServer.writeBytes(msgToServer);//sending the message
 		  
 		  System.out.println("sending to Arduino-Server: "+msgToServer);//debug
 		  		  	      
-	      if((msgFromServer = inFromServer.readLine()) != null){
-		      System.out.println("received from Arduino-Server: " + msgFromServer);//print the answer
-	      }
+	      //if((msgFromServer = inFromServer.readLine()) != null){
+		  //    System.out.println("received from Arduino-Server: " + msgFromServer);//print the answer
+	      //}
 	      
 		  clientSocket.close();
 		  System.out.println("Disconnected from:"+serverIP);//debug
