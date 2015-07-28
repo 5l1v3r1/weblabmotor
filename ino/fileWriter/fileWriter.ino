fec0cc9c1a4510b65b1c301579c7ec29718cefd9
Bruno Stabile <brn.stabile@gmail.com>
2015-07-27 21:30:21 -0300

    create file json using c++

--- ino/fileWriter/fileWriter.ino
+++ ino/fileWriter/fileWriter.ino
@@ -4,8 +4,6 @@ void setup() {
   //root.printTo(buffera, sizeof(buffera));
   //pFile = fopen ("/home/root/myfile.json", "a");
   //fwrite (buffera , sizeof(char), sizeof(buffera), pFile);
-
-  root.printTo(Serial);
   
 //[{"DATA": ["2015-07-27 21:16:05", "2015-07-27 21:16:05", "2015-07-27 21:16:06", "2015-07-27 21:16:06"]}, {"A0": [0, 0, 0, 0]}, {"A1": [2, 2, 2, 2]}]
 
