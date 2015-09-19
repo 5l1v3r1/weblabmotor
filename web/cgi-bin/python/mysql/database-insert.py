61fc5b4353237d1247fe3b1c136d0712ea6812a7
Bruno Stabile <brn.stabile@gmail.com>
2015-09-19 15:57:19 -0300

    Insert cgi python ok

--- web/cgi-bin/python/mysql/database-insert.py
+++ web/cgi-bin/python/mysql/database-insert.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-import cgi, cgitb, MySQLdb, sys
+import cgi, cgitb, sys
+import MySQLdb as mdb
 
 #habilita debug no browser
 cgitb.enable();
 
-dados = cgi.FieldStorage() 
+dados = cgi.FieldStorage()
 
-#bd = dados.getvalue('bd')
 a0 = dados.getvalue('a0');
 a1 = dados.getvalue('a1');
 a2 = dados.getvalue('a2');
@@ -15,22 +16,16 @@ a3 = dados.getvalue('a3');
 a4 = dados.getvalue('a4');
 a5 = dados.getvalue('a5');
 
-# Connect to MySQL database
-db = MySQLdb.connection(host="127.0.0.1",user="root",passwd="root",db="sensores")
 
-#db.query("INSERT INTO 'sensores'.'sensores'('data','a0','a1','a2','a3','a4','a5')VALUES(CURRENT_TIMESTAMP,?,?,?,?,?,?);",(a0,a1,a2,a3,a4,a5))
+con = mdb.connect('localhost', 'root', 'root', 'sensores');
 
-db.query("INSERT INTO `sensores`.`sensores`(`data`,`a0`,`a1`,`a2`,`a3`,`a4`,`a5`) VALUES(CURRENT_TIMESTAMP,1,3,4,5,6,7);")
-
-#conn = sqlite3.connect(bd)
-#c = conn.cursor()                            
-
-#c.execute("INSERT INTO sensores VALUES (datetime('now','localtime'),?,?,?,?,?,?,'1','0')",(a0,a1,a2,a3,a4,a5))
-
-#conn.commit()
-#conn.close()
+with con:
+    
+    cur = con.cursor()
+    cur.execute("INSERT INTO `sensores`.`dados`(`data`,`a0`,`a1`,`a2`,`a3`,`a4`,`a5`)VALUES(current_timestamp(),%s,%s,%s,%s,%s,%s);",(a0,a1,a2,a3,a4,a5))
 
 print "Status: 200 OK"
 print "Content-Type: text/html"
 print ""
 print "%s" %(dados)
+print "<br>"
