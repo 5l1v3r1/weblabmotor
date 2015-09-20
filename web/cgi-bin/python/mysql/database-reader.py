f6d974df1e9951a15e56830e49a117c0072b3724
root <root@raspberry-pi.(none)>
2015-09-20 18:48:36 -0300

    read database mysql

--- web/cgi-bin/python/mysql/database-reader.py
+++ web/cgi-bin/python/mysql/database-reader.py
@@ -1,48 +1,22 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-# Import modules for CGI handling 
-import cgi, cgitb, sqlite3, sys, json 
+import cgi, cgitb, sys, json
+import MySQLdb as mdb
+cgitb.enable();
 
-# Create instance of FieldStorage 
-bd = cgi.FieldStorage() 
-
-# Get data from fields
-bd = bd.getvalue('bd')
-
-def dict_factory(cursor, row):
-    d = {}
-    for idx, col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
- 
-connection = sqlite3.connect(bd)
-connection.row_factory = dict_factory
+connection = mdb.connect('localhost', 'root', 'root', 'sensores');
  
 cursor = connection.cursor()
 
-cursor.execute("SELECT * FROM sensores ORDER BY date ASC")
+cursor.execute("SELECT * FROM dados ORDER BY data ASC")
  
 dados = cursor.fetchall()
-
-A0 = []
-A1 = []
-DATA = []
-
-for x in xrange((len(dados))):
-	A0.append(dados[x]['a0'])
-	A1.append(dados[x]['a1'])
-	DATA.append(dados[x]['date'])
-
-dic_A0 = {'A0':(A0)}
-dic_A1 = {'A1':(A1)}
-dic_DATA = {'DATA':(DATA)}
-
-dados = [dic_DATA,dic_A0,dic_A1]
-dadosJson = json.dumps(dados)
+dados = [i for sub in dados for i in sub]  # Convert to list from tuple
 
 connection.close() 
 
 print "Status: 200 OK"
-print "Content-Type: application/json"
+print "Content-Type: text/html"
 print ""
-print "%s" % (dadosJson)
+print "%s" % (dados)
