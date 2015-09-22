cac0c34d0d8c293b70d45355ddbe5aa487573fd1
root <root@raspberry-pi.(none)>
2015-09-22 01:12:03 -0300

    php insert with parameters

--- web/cgi-bin/php/mysql/database-insert.php
+++ web/cgi-bin/php/mysql/database-insert.php
@@ -1,13 +1,19 @@
 <?php
   try
   {
+
+  	$a0 = $_GET["a0"];
+	$a1 = $_GET["a1"];
+  	$a2 = $_GET["a2"];
+	$a3 = $_GET["a3"];
+  	$a4 = $_GET["a4"];
+	$a5 = $_GET["a5"];
+
     //open the database
 	$db = new PDO('mysql:host=localhost;dbname=sensores','root','root');
 
-    $sql = "INSERT INTO dados (data, a0, a1, a2, a3, a4, a5)
-    VALUES (current_timestamp(),'1', '2', '3', '4', '5' , '3')";
-
-    $db->exec($sql);
+    $db->exec("INSERT INTO dados (data, a0, a1, a2, a3, a4, a5)
+    VALUES (current_timestamp(),'$a0', '$a1', '$a2', '$a3', '$a4' , '$a5')");
 
     // close the database connection
     $db = NULL;
