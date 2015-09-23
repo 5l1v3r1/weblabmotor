1530bb1c99c1418e4ae5577429971c78f66f55eb
Bruno Stabile <brn.stabile@gmail.com>
2015-09-23 15:58:39 +0000

    shell script packages added

--- sh/install.sh
+++ sh/install.sh
@@ -10,12 +10,22 @@ case ${option} in
       ;; 
    -d) DIR="${2}" 
       echo "Configurando diretorios de acesso web para linux debian like"
-          sudo ln -s -T /opt/weblabmotor/web/ /var/www/html/home
-          sudo chown -R www-data.www-data /var/www/html/home
-          sudo chmod -R 755 /var/www/html/home
-      ;; 
+          sudo ln -s -T /opt/weblabmotor/web/ /var/www/home
+          sudo chown -R www-data.www-data /var/www/home
+          sudo chmod -R 755 /var/www/home/
+      ;;
+    -i) DIR="${2}" 
+        echo "instalando pacotes para linux debian like"
+        sudo apt-get update
+        sudo apt-get install php5 apache2 mysql-server php5-mysql phpmyadmin
+      ;;
+    -r) DIR="${2}" 
+        echo "instalando pacotes para Rpi"
+        apt-get update
+        apt-get install php5 apache2 mysql-server php5-mysql phpmyadmin
+      ;;
    *)  
-      echo "`basename ${0}`:usage: [-y yocto] | [-d debianLike]" 
+      echo "`basename ${0}`:usage: [-y yocto] | [-d debianLike] [-i installDebianLike] [-r installRpi]" 
       exit 1 # Command to come out of the program with status 1
       ;; 
 esac 
