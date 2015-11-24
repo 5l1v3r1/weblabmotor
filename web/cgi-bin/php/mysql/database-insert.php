b8d50315235f3bb76d519135cc99f8b16204035c
Bruno Stabile <brn.stabile@gmail.com>
2015-11-23 23:35:00 -0200

    Update database-insert.php

--- web/cgi-bin/php/mysql/database-insert.php
+++ web/cgi-bin/php/mysql/database-insert.php
@@ -18,9 +18,6 @@
 
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    //$db->exec("INSERT INTO usr (data, a0, a1, a2, a3, a4, a5)
-    //VALUES (current_timestamp(),'$a0', '$a1', '$a2', '$a3', '$a4' , '$a5')");
-
     $stmt = $conn->prepare("INSERT INTO usrTemporario ( a0, a1, a2, a3, a4, a5, a6, a7, a8, a9)
     VALUES (:a0, :a1, :a2, :a3, :a4, :a5, :a6, :a7, :a8, :a9)");
     $stmt->bindParam(':a0', $a0);
@@ -29,8 +26,10 @@
     $stmt->bindParam(':a3', $a3);
     $stmt->bindParam(':a4', $a4);
     $stmt->bindParam(':a5', $a5);
-    $stmt->bindParam(':a5', $a5);
-    $stmt->bindParam(':a5', $a5);
+    $stmt->bindParam(':a6', $a6);
+    $stmt->bindParam(':a7', $a7);
+    $stmt->bindParam(':a8', $a8);
+    $stmt->bindParam(':a9', $a9);
 
     $stmt->execute();
 
